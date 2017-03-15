1bf3b42ab87fd567c7fc586723f74dc7a892a084
Yusef Habib <habibfernandez@gmail.com>
2017-03-15 10:26:54 +0100

    Cleaned main

--- src/main/java/events/EventsRestApi.java
+++ src/main/java/events/EventsRestApi.java
@@ -1,27 +1,11 @@
 package events;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class EventsRestApi {
 
-//	@Bean
-//	public DataSource dataSource() {
-//	  String url = "jdbc:h2:tcp://localhost/~/h2-events-app";
-//	  return new DriverManagerDataSource(url);
-//	}
-//	
-//	@Bean
-//	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-//	    return new JdbcTemplate(dataSource());
-//	}
-	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EventsRestApi.class, args);
 	}
